f8126d340342d9e40ac8da4de98e61a0c5bdf2ee
Nick Kim <nicholaskim1989@gmail.com>
2020-08-01 19:07:12 -0700

    testing without mongodb

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://local/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// var MONGODB_URI = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 
